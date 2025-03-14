3d4f3febe7cee14e770ee74ab108da004fa6da80
karan <karanchuk03@gmail.com>
2025-03-14 17:41:47 +0200

    saucedemo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,5 @@
 package org.example;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import org.example.Configuration.ConfigReader;
-
 public class Main {
 
     public static void main(String[] args) {
